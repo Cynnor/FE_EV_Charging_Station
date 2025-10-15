98877477feb8383092fe3d1018831a4629b4f784
ThangTM-2K4 <thangtmse184159@fpt.edu.vn>
2025-10-16 02:50:56 +0700

    call api & fix scss

--- src/config/api.jsx
+++ src/config/api.jsx
@@ -2,22 +2,16 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://private-eve-evchargingstation-7d82d2a9.koyeb.app",
-  headers: {
-    "Content-Type": "application/json",
-  },
+  headers: { "Content-Type": "application/json" },
 });
 
 api.interceptors.request.use(
-  function (config) {
+  (config) => {
     const token = localStorage.getItem("token");
-    config.headers.Authorization = `Bearer ${token}`;
+    if (token) config.headers.Authorization = `Bearer ${token}`;
     return config;
   },
-
-  function (error) {
-    // Do something with request error
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 export default api;
